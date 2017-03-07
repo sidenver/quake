5ce06c4cd4a7ec09a3ada565d59ff6935206076f
sidenver <hanchinshing@gmail.com>
2017-03-07 14:00:53 -0500

    run with 1 core

--- run.sh
+++ run.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
-#SBATCH --ntasks=16
+#SBATCH --ntasks=1
 #SBATCH -t 00:00:30
 #SBATCH --mem-per-cpu=2048
 #SBATCH --exclusive
 
 OMP_NUM_THREADS=$SLURM_NTASKS
+echo $OMP_NUM_THREADS
 export OMP_NUM_THREADS
 
+./quake quake.in
