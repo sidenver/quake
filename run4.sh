fb94783b2c95d5b6e42d3a7859789a4090ea7e93
sidenver <hanchinshing@gmail.com>
2017-03-12 12:04:41 -0400

    parallel 4 way

---
+++ run4.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#SBATCH --ntasks=4
+#SBATCH -t 00:10:00
+#SBATCH --mem-per-cpu=2048
+#SBATCH --share
+#SBATCH -p debug
+
+OMP_NUM_THREADS=4
+echo $OMP_NUM_THREADS
+export OMP_NUM_THREADS
+
+./quake < quake.in &> quake4.log
